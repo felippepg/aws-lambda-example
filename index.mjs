ca67e0cffd8fd810bca3966c478c9713f6694ce9
felippepg <felippepg.goncalves@outlook.com.br>
2024-01-05 18:08:19 -0300

    corrigindo nome Records

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { S3 } from '@aws-sdk/client-s3';
 
 const s3Client = new S3({ region: 'us-east-2' });
 export const handler = async (event) => {
-  const record = event.Record[0];
+  const record = event.Records[0];
   const Bucket = record.s3.bucket.name;
   const Key = record.s3.object.key;
   const getObjetctResult = await s3Client.getObject({
