770fc0e24bc82859da4819098d8b6f23b20d3585
felippepg <felippepg.goncalves@outlook.com.br>
2024-01-05 18:00:05 -0300

    Buscando objetos do S3

--- index.mjs
+++ index.mjs
@@ -1,7 +1,21 @@
+import { S3 } from '@aws-sdk/client-s3';
+
+const s3Client = new S3({ region: 'us-east-2' });
 export const handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Escrito pelo VSCODE e enviado via Github Actions'),
-  };
-  return response;
+  const record = event.Record[0];
+  const Bucket = record.s3.bucket.name;
+  const Key = record.s3.object.key;
+  const getObjetctResult = await s3Client.getObject({
+    Bucket,
+    Key,
+  });
+  const megaByte = 1024 * 1024;
+
+  if (getObjetctResult.ContentLength > 1 * megaByte) {
+    console.log('Objeto muito grande');
+    return 'Objeto muito grande';
+  }
+
+  console.log('Objeto de tamanho ok');
+  return 'Objeto de tamanho ok';
 };
