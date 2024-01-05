babedb764cca93c26e010ca7e286332438cb2bcb
felippepg <felippepg.goncalves@outlook.com.br>
2024-01-05 16:24:56 -0300

    Atualizando index

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Escrito pelo VSCODE'),
+    body: JSON.stringify('Escrito pelo VSCODE e enviado via Github Actions'),
   };
   return response;
 };
